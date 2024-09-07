d49e7d86149f0e3e60cfd369ae5ee2a8adb36abe
Yannis <yannisp70@gmail.com>
2024-09-07 17:49:29 +0100

    +kitty

---
+++ home-manager/modules/kitty.nix
@@ -0,0 +1,10 @@
+{
+    programs.kitty = {
+        enable = true;
+        extraConfig = ''
+            # Set the default window size
+            initial_window_width  = 100;
+            initial_window_height = 40;
+        '';
+    };
+}
