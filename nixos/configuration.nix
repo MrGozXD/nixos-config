a859bc3ed5d6135bcac620fb722e5a404b45a7aa
Yannis <yannisp70@gmail.com>
2025-04-27 16:27:38 +0100

    localbinInPath

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,6 +16,8 @@
     ./modules/bundle.nix
   ];
 
+  environment.localBinInPath = true; # Add ~/.local/bin to PATH
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
